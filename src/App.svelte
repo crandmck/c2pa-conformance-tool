ba20a6bcde3b34c96e51798663ead7ca0d0cc14d
Andy LeMay <lemay@adobe.com>
2026-03-17 11:13:22 -0500

    Adding DM Sans
    Swapping to DM Sans from Google Fonts for consistency with https://c2pa.org/

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,9 @@
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" >
+  <link href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100..1000;1,9..40,100..1000&display=swap" rel="stylesheet">
+</svelte:head>
+
 <script lang="ts">
   import { onMount } from 'svelte'
   import FileUpload from './lib/FileUpload.svelte'
